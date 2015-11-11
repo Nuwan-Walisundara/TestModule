5e4952bbfdf22b7053cd37064483e92668fdcf05
Nuwan <nuwan@wso2telco.com>
2015-11-11 16:52:17 +0530

    rollback chenges

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,12 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Heo World!" );
-=======
         System.out.println( "Hello World!" );
 
-System.out.println( "Hello World!!!!!!!!!!!!!!!!!!!" );
->>>>>>> a7f74ec2218f55bc64bd8d5dbd1d7acec617fd24
     }
 }
